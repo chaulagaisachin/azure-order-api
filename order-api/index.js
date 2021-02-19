43345cec2528cb570b5ffe25e4a2aa349c96123d
Sachin Chaulagai <50958807+chaulags@users.noreply.github.com>
2021-02-19 10:34:39 +0545

    MASTER

---
+++ order-api/index.js
@@ -0,0 +1,20 @@
+const { ServiceBusClient } = require("@azure/service-bus");
+
+const serviceBusClient = new ServiceBusClient("Endpoint=sb://messagebroker-sachin.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=yO/0+9OdglEsq8ydshZYyJF+Cy3Ku2uXQpgenUfp1fI=");
+const sender = serviceBusClient.createSender("hello");
+
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const number = (req.query.number || (req.body && req.body.number));
+    await sender.sendMessages({"body" : number});
+    const responseMessage = number
+        ? "Hello, " + number + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a number in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
